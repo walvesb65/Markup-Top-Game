3e2077e5541e68622d80e2430746dd2a0be5f0e5
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 01:24:20 -0300

    Feat: Create getGenres and getPublishers

--- src/services/server.js
+++ src/services/server.js
@@ -5,6 +5,18 @@ function getPlatforms() {
   return Array.from(platforms)
 }
 
+function getGenres() {
+  const genres = new Set(dbjson.map(({ Genre }) => Genre));
+  return Array.from(genres)
+}
+
+function getPublishers() {
+  const publishers = new Set(dbjson.map(({ Publisher }) => Publisher));
+  return Array.from(publishers)
+}
+
 module.exports = {
   getPlatforms,
+  getGenres,
+  getPublishers,
 }
