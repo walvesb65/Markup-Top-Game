8b8c21f2d004613ff4fef6364dce32541327e4c5
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 14:18:43 -0300

    Refactor: AuthProvider

--- src/provider/context.js
+++ src/provider/context.js
@@ -4,12 +4,29 @@ export const AuthContext = React.createContext({})
 
 export const AuthProvider = (props) => {
 
-  const [user, setUser] = useState({
-    name: "will",
-  })
+  const [filter, setFilter] = useState({
+    Platform: [],
+    Genre: [],
+    Publisher: []
+  });
+
+  const upDateFilter = (type, value) => {
+    if (filter[type].includes(value)) {
+      const removedFilter = filter[type].filter((e) => e !== value);
+      setFilter({
+        ...filter,
+        [type]:removedFilter,
+      })
+    } else {
+      setFilter({
+        ...filter,
+        [type]:[...filter[type], value]
+      })
+    }
+  }
 
   return (
-  <AuthContext.Provider value={{user, setUser}}>
+  <AuthContext.Provider value={{upDateFilter}}>
     {props.children}
   </AuthContext.Provider>)
 }
