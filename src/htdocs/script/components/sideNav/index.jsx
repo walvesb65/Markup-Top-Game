d56986e6784a6e53055abf64ca96d4a358b7dd88
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 00:13:54 -0300

    Fix: add label´s key

--- src/htdocs/script/components/sideNav/index.jsx
+++ src/htdocs/script/components/sideNav/index.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
 import dbJson from '../../../data.json'
+import './styles.css';
 
 export default function Sidenav() {
 
   const { platform, gender, developer } = dbJson;
-  console.log(platform);
+
   return (
     <div className="container_side_nav">
-      <fieldset className="select_platform">
-        {platform.map((data) => (
-          <label htmlFor={data}>
-            {data}
-            <input type="checkbox" name={data} id={data} />
-          </label>
-        ))}
-      </fieldset>
-      <fieldset className="select_gender">
-        {gender.map((data) => (
-          <label htmlFor={data}>
-            {data}
-            <input type="checkbox" name={data} id={data} />
-          </label>
-        ))}
-      </fieldset>
-      <fieldset className="select_developer">
-        {developer.map((data) => (
-          <label htmlFor={data}>
-            {data}
-            <input type="checkbox" name={data} id={data} />
-          </label>
-        ))}
-      </fieldset>
-      <div className="buttons">
+      <div className="dropDown">
+        <fieldset className="select_platform">
+          {platform.map((data) => (
+            <label htmlFor={data} key={data}>
+              {data}
+              <input type="checkbox" name={data} id={data} />
+            </label>
+          ))}
+        </fieldset>
+        <fieldset className="select_gender">
+          {gender.map((data) => (
+            <label htmlFor={data} key={data}>
+              {data}
+              <input type="checkbox" name={data} id={data} />
+            </label>
+          ))}
+        </fieldset>
+        <fieldset className="select_developer">
+          {developer.map((data) => (
+            <label htmlFor={data} key={data}>
+              {data}
+              <input type="checkbox" name={data} id={data} />
+            </label>
+          ))}
+        </fieldset>
+      </div>
+      <div className="buttons_sideNav">
         <button>Desfazer Filtros</button>
         <button>Download IMG</button> 
       </div>
