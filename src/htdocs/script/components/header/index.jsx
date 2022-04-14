eaf3d84e4f138c16a64025f18b73cc72fdf69443
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 14:20:37 -0300

    Fix: Header page

--- src/htdocs/script/components/header/index.jsx
+++ src/htdocs/script/components/header/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { AuthContext } from '../../../../provider/context';
 import Logo from '../../../assets/media/globo_logo.png';
 import './styles.css'
 
 export default function Header() {
-
-  const { user } = React.useContext(AuthContext)
   return (
     <div className="container_header">
       <div className="header_Left">
         <img src={Logo} alt="Logo da rede Globo" />
-        <h1>{user.name}</h1>
+        <h1>INFOGames</h1>
       </div>
       <div className="header_Right">
         <p className="user_name">User Name</p>
