e63455ec3189343f19549b7818a94a6cad5d8ef4
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 18:05:03 -0300

    Style: Position attributes and remove header Styles

--- src/htdocs/script/components/sideNav/index.jsx
+++ src/htdocs/script/components/sideNav/index.jsx
@@ -17,25 +17,45 @@ export default function Sidenav() {
       <div className="drop_Down">
         <fieldset className="select_platform">
           {getPlatforms().map((data) => (
-            <label htmlFor={data} key={data}>
-              {data}
-              <input type="checkbox" name="Platform" id={data} value={data} onClick={handleClick}/>
+            <label htmlFor={data.Platform} key={data.key}>
+              {data.Platform}
+              <input
+                type="checkbox"
+                name="Platform"
+                id={data.Platform}
+                defaultChecked={data.checked}
+                value={data.Platform}
+                onClick={handleClick}
+              />
             </label>
           ))}
         </fieldset>
         <fieldset className="select_gender">
           {getGenres().map((data) => (
-            <label htmlFor={data} key={data}>
-              {data}
-              <input type="checkbox" name="Genre" id={data} value={data} onClick={handleClick}/>
+            <label htmlFor={data.Genre} key={data.key}>
+              {data.Genre}
+              <input
+                type="checkbox"
+                name="Genre"
+                id={data.Genre}
+                defaultChecked={data.checked}
+                value={data.Genre}
+                onClick={handleClick}
+              />
             </label>
           ))}
         </fieldset>
         <fieldset className="select_developer">
           {getPublishers().map((data) => (
-            <label htmlFor={data} key={data}>
-              {data}
-              <input type="checkbox" name="Publisher" id={data} value={data} onClick={handleClick}/>
+            <label htmlFor={data.Publisher} key={data.key}>
+              {data.Publisher}
+              <input
+                type="checkbox"
+                name="Publisher"
+                id={data.Publisher}
+                defaultChecked={data.checked}
+                value={data.Publisher}
+                onClick={handleClick}/>
             </label>
           ))}
         </fieldset>
