bf90cf3c52cfb88c4383c4c9e77118fd1670d494
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 18:00:40 -0300

    Style: Remove input test and useContext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import '../src/htdocs/style/globalstyle.css'
 import MarkupPage from "./htdocs/script/pages/MarkupPage/index";
-import { AuthContext } from './provider/context';
 
 function App() {
-
-  const { user, setUser } = React.useContext(AuthContext)
-  console.log(user);
   
   return (
     <div className="App">
-      <input
-        type="text"
-        onChange={(e) => setUser({name: e.target.value})}
-      ></input>
       <MarkupPage />
     </div>
   );
