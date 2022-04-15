75533017df1b06987cf08394fee940d1e86c3c0d
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 23:04:32 -0300

    Feat: Add new import Game Ranking

--- src/htdocs/script/components/markup/index.jsx
+++ src/htdocs/script/components/markup/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DateSeciton from './deteSection';
+import GamesRanking from './topGamesSection';
 
 export default function Markup() {
   return (
     <div className="container_markup">
       <DateSeciton />
+      <GamesRanking />
     </div>
   )
 }
