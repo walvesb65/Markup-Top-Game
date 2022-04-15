2a2630693781dfb45fd29c4645f83ae15c96bf4a
Willian Alves <willian.alvesb65@gmail.com>
2022-04-14 23:06:04 -0300

    Feat: add useReport

---
+++ src/htdocs/script/components/markup/topGamesSection/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import useReport from '../../../hooks/useReport'
+
+export default function GamesRanking() {
+
+  const { Ranking } = useReport();
+  console.log(Ranking);
+  return (
+    <div className="container_top_game">
+      <h1>Top 10 Games</h1>
+      <div className="table">
+      <table>
+        <thead>
+          <tr>
+            <th>Ranking</th>
+            <th>Nome</th>
+            <th>Vendas Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Ranking.length && Ranking.map((e, index)=> (
+          <tr key={e.name}>
+            <td>{index + 1}</td>
+            <td>{e.name}</td>
+            <td>{e.globalSales}</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  )
+}
