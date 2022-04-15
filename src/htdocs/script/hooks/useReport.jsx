7fb62c8aeb2b776bc9a030f1a44aa17cb9711ded
Willian Alves <willian.alvesb65@gmail.com>
2022-04-15 19:21:29 -0300

    Feat: Create func getValuesSales

--- src/htdocs/script/hooks/useReport.jsx
+++ src/htdocs/script/hooks/useReport.jsx
@@ -6,21 +6,39 @@ export default function useReport() {
   const { filteredData } = useContext(AuthContext)
   const [reportData, setReportData] = useState({
     Ranking: [],
+    Sales: {}
   });
   
   const getMostSaledGames = () => {
     return filteredData.sort((a,b) => (
-      parseFloat(a.Global_Sales) > parseFloat(b.Global_Sales)
+      parseFloat(a.globalSales) > parseFloat(b.globalSales)
     )? -1 : 1).slice(0,10)
   };
 
+  const getValuesSales = () => {
+    if (filteredData.length > 0){
+      return filteredData.reduce((Sales, { globalSales, NASales, EUSales, JPSales, otherSales }) => ({
+        globalSales: parseFloat(globalSales) + parseFloat(Sales.globalSales),
+        NASales: parseFloat(NASales) + parseFloat(Sales.NASales),
+        EUSales: parseFloat(EUSales) + parseFloat(Sales.EUSales),
+        JPSales: parseFloat(JPSales) + parseFloat(Sales.JPSales),
+        otherSales: parseFloat(otherSales) + parseFloat(Sales.otherSales)
+      }))
+    }
+    return {};
+  }
+
   const updateReporteData = () => {
-    setReportData({ Ranking: getMostSaledGames() });
+    setReportData({
+      Ranking: getMostSaledGames(),
+      Sales: getValuesSales()
+    });
   }
   
   useEffect(() => {
     updateReporteData();
   }, [filteredData])
+
 
   return {
     ...reportData,
