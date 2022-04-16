6a6ab23751d1c52d5a46fa7c8ad75529ba076e0c
Willian Alves <willian.alvesb65@gmail.com>
2022-04-16 20:47:26 -0300

    Fix: Remove generator unique id

--- src/services/server.js
+++ src/services/server.js
@@ -2,19 +2,19 @@ const dbjson = require('../data.json');
 
 function getplatforms() {
   const platforms = new Set(dbjson.map(({ platform }) => platform));
-  const setCheck = Array.from(platforms).map((e) => ({ platform: e, key: Math.floor(Date.now() * Math.random()).toString(36) }))
+  const setCheck = Array.from(platforms).map((e) => ({ platform: e}))
   return setCheck;
 }
 
 function getgenres() {
   const genres = new Set(dbjson.map(({ genre }) => genre));
-  const setCheck = Array.from(genres).map((e) => ({ genre: e, key: Math.floor(Date.now() * Math.random()).toString(36) }))
+  const setCheck = Array.from(genres).map((e) => ({ genre: e}))
   return setCheck;
 }
 
 function getpublishers(value) {
   const publishers = new Set(dbjson.map(({ publisher }) => publisher));
-  const setCheck = Array.from(publishers).map((e) => ({ publisher: e, key: Math.floor(Date.now() * Math.random()).toString(36) }))
+  const setCheck = Array.from(publishers).map((e) => ({ publisher: e}))
   
   if (value) {
     const regexValue = new RegExp(`\\w*${value}\\w*`,'i');
