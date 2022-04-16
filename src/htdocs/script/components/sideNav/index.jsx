c58a8dcfabff1af5151b256ebb9ac3d502cae1a6
Willian Alves <willian.alvesb65@gmail.com>
2022-04-16 20:46:14 -0300

    Fix : create new state drop down

--- src/htdocs/script/components/sideNav/index.jsx
+++ src/htdocs/script/components/sideNav/index.jsx
@@ -1,19 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import './styles.css';
+import classNames from 'classnames';
 import { getplatforms, getgenres, getpublishers } from '../../../../services/server';
+import { ReactComponent as Arrow } from '../../../assets/media/arrowdrop.svg'
 import useFilter from '../../hooks/useFilter';
+import './styles.scss';
 
 export default function Sidenav() {
 
   const { updateFilterOptions, clearFilterOptions } = useFilter()
-  const handleClick = ({ target }) => {
+  const handleClickOptions = ({ target }) => {
     const { name, value } = target
     updateFilterOptions(name, value);
   }
-
+  const [isShow, setIsShow] = useState({
+    platform: false,
+    genre: false,
+    publisher: false
+  });
+  // const [showGenre, setShowGenre] = useState(false);
+  // const [showDeveloper, setShowDeveloper] = useState(false);
   const [value, setValue] = useState("")
   useEffect(() => {
   }, [value])
+
+const handleClickDrop = (type) => {
+  setIsShow({
+    ...isShow,
+    [type]: !isShow[type]
+  });
+} 
 
   const handlechange = ({ target: { value } }) => {
     setValue(
@@ -23,62 +38,78 @@ export default function Sidenav() {
 
   return (
     <div className="container_side_nav">
-      <div className="drop_Down_platform">
-        <fieldset className="select_platform">
-          {getplatforms().map((data) => (
-            <label
-              htmlFor={data.platform}
-              key={data.key}
-            >
-              {data.platform}
-              <input
-                type="checkbox"
-                name="platform"
-                id={data.platform}
-                value={data.platform}
-                onChange={handleClick}
-              />
-            </label>
-          ))}
-        </fieldset>
+      <div className="platform">
+        <div onClick={() => handleClickDrop('platform')} className="drop_title">
+          <h1>Plataforma</h1>
+          <Arrow className="arrow_icon" />
+        </div>
+        <div className={classNames('drop_Down',{hidden: isShow.platform})}>
+          <ul className="select_platform">
+            {getplatforms().map((data) => (
+              <li key={data.platform}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="platform"
+                    id={data.platform}
+                    value={data.platform}
+                    onChange={handleClickOptions}
+                  />
+                  <p>{data.platform}</p>
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="drop_Down_gender">
-        <fieldset className="select_gender">
-          {getgenres().map((data) => (
-            <label
-              htmlFor={data.genre}
-              key={data.key}
-            >
-              {data.genre}
-              <input
-                type="checkbox"
-                name="genre"
-                id={data.genre}
-                value={data.genre}
-                onChange={handleClick}
-              />
-            </label>
-          ))}
-        </fieldset>
+      <div className="genre">
+        <div onClick={() => handleClickDrop('genre')} className="drop_title">
+          <h1>Gênero</h1>
+          <Arrow className="arrow_icon" />
+        </div>
+        <div className={classNames('drop_Down',{hidden: isShow.genre})}>
+          <ul className="select_gender">
+            {getgenres().map((data) => (
+              <li key={data.genre}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="genre"
+                    id={data.genre}
+                    value={data.genre}
+                    onChange={handleClickOptions}
+                  />
+                  <p>{data.genre}</p>
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="drop_Down_developer">
-        <fieldset className="select_developer">
+      <div className="developer">
+        <div onClick={() => handleClickDrop('publisher')} className="drop_title">
+          <h1>Desenvolvedora</h1>
+          <Arrow className="arrow_icon" />
+        </div>
+        <div className={classNames('drop_Down',{hidden: isShow.publisher})}>
           <input type="text" onChange={handlechange} />
-          {getpublishers(value).map((data) => (
-            <label
-              htmlFor={data.publisher}
-              key={data.key}>
-              {data.publisher}
-              <input
-                type="checkbox"
-                name="publisher"
-                id={data.publisher}
-                value={data.publisher}
-                onChange={handleClick}
-              />
-            </label>
-          ))}
-        </fieldset>
+          <ul className="select_developer">
+            {getpublishers(value).map((data) => (
+              <li key={data.publisher}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="publisher"
+                    id={data.publisher}
+                    value={data.publisher}
+                    onChange={handleClickOptions}
+                  />
+                  <p>{data.publisher}</p>
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="buttons_sideNav">
         <button
