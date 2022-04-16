50bc6b04ec9cd2c4d6411c70f45e0a00b97b97ad
Willian Alves <willian.alvesb65@gmail.com>
2022-04-16 15:05:00 -0300

    Style: Line Chart

--- src/htdocs/script/components/markup/historicSection/index.jsx
+++ src/htdocs/script/components/markup/historicSection/index.jsx
@@ -5,11 +5,10 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 export default function ChartSales() {
 
   const { Historic } = useReport()
-  // const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
 
   return (
   <LineChart width={600} height={300} data={Historic} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-    <Line type="monotone" dataKey="Sales" stroke="#8884d8" />
+    <Line dataKey="Sales" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
     <XAxis dataKey="Year" />
     <YAxis />
